5eda093f55a4eed27484a54a6d286bd04ea83935
Mohammad Akram <khanakram32392@gmail.com>
2025-04-30 16:27:31 +0530

    console log

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -7,6 +7,9 @@ export const verifyJWT = asyncHandler(async(req, res ,next)=>{
     try {
         const token = req.header('Authorization')?.replace('Bearer ', '') || req.cookies?.accessToken
 
+        console.log(token)
+        
+
         if (!token) {
             throw new ApiError(401, 'Unauthorized Request!')
         }
